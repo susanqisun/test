115218f39a222e4c51474e5363785f13121ae0ba
susanqisun <46974108+susanqisun@users.noreply.github.com>
2021-04-19 17:51:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit.components.v1 as components
 
 st.header("test html import")
 
-HtmlFile = open("test.html", 'r', encoding='utf-8')
+HtmlFile = open("ratingtest02.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 print(source_code)
 components.html(source_code)
