ca09393fa8514335a1da2cf6c261ede1c3067dc8
susanqisun <46974108+susanqisun@users.noreply.github.com>
2021-04-21 00:25:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,23 +12,7 @@ movie_title = st.selectbox(
 
 test = st.button('Search for recommended movies')
 
-#run recommender
-if test:
-	if movie_title:
-		#spinner
-		#below lines show we are done
-		#if len(climb_id)>10:
-			#climb_id = climb_id.split('/')[-2]
-		#else we have climb id lets look it up
-		#st.success('Searching for similar climbs in that area')
-		#call function and pass id, city, state, zip and radius
-		#fxn returns df of 10 most similar climbs in search range
-		st.dataframe(models_app.recommendations02(title=movie_title))
-		st.success('Finished')
-		st.balloons()
-		#RUN recommender
-	else:
-		st.error('Please select a movie')
+
 
 
 
